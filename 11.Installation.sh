6038d9588e3394d69df9c59a27ce4e8d2bbde05b
VinayKumar <vinaydarla832@gmail.com>
2026-04-01 21:08:23 +0530

    Added script to install mysql on server

---
+++ 11.Installation.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -eq 0 ]; then
+    echo "You are root user, you can install the software"
+else
+    echo "Please run this with Root Privileges"
+fi
+
+dnf install mysql -y
+if [ $? -eq 0 ]; then
+    echo "MySQL installed successfully"
+else
+    echo "Failed to install MySQL"
+fi
