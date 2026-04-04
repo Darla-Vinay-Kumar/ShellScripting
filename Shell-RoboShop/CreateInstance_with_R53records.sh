6fa4b26de80fc9085e7fcb1c94444796c8d905e3
VinayKumar <vinaydarla832@gmail.com>
2026-04-04 18:24:44 +0530

    updated

--- Shell-RoboShop/CreateInstance_with_R53records.sh
+++ Shell-RoboShop/CreateInstance_with_R53records.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 AMI_ID="ami-0220d79f3f480ecf5"
 SG_ID="sg-06298c18e86c57b2e"
-
+ZoneId="Z018042118DWYK39M0YVT"
+DomainName="darlavinaykumar.fun"
 for instance in $@
 do
     echo "Creating $instance instance"
@@ -11,9 +12,27 @@ do
     if [ "$instance" != "Frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $InstanceID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         echo "Private IP of $instance instance: $IP"
+        RecordName="$instance.$DomainName"
     else
         IP=$(aws ec2 describe-instances --instance-ids $InstanceID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         echo "Public IP of $instance instance: $IP"
+        RecordName="$DomainName"
 
     fi
+    aws route53 change-resource-record-sets\
+    --hosted-zone-id $ZoneId \
+    --change-batch '
+    {
+    "Comment": "Added record for '$instance'",
+    "Changes": [{
+    "Action"                  :"UPSERT",
+    "ResourceRecordSet"       :{
+    "Name"                    :"'$RecordName'",
+    "Type"                    :"A",
+    "TTL"                     :1,
+    "ResourceRecords"         :[{
+    "Value"                   :"'$IP'"
+    }]
+    }
+    }]}'
 done
