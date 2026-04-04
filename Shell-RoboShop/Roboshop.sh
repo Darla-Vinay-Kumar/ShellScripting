7ae0a9e758bb3af6593722a8dca77cf0c15d9b84
VinayKumar <vinaydarla832@gmail.com>
2026-04-04 18:03:07 +0530

    script modified

--- Shell-RoboShop/Roboshop.sh
+++ Shell-RoboShop/Roboshop.sh
@@ -5,7 +5,7 @@ SG_ID="sg-06298c18e86c57b2e"
 for instance in $@
 do
     echo "Creating $instance instance"
-    InstanceID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    InstanceID=$(aws ec2 run-instances --image-id "ami-0220d79f3f480ecf5" --instance-type t3.micro --security-group-ids "sg-06298c18e86c57b2e" --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     echo "Instance created with ID: $InstanceID"
 
     if [ "$instance" != "Frontend" ]; then
